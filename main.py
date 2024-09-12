4b7b6bbbd4ef2da4be58765c829990a1218aa565
shivaywadhawan <shivaywadhawan@gmail.com>
2024-09-12 01:14:54 -0400

    Setup virtual env and streamlit

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+
+def main():
+    # Set page config
+    st.set_page_config(page_title='Groq voicebot', page_icon='🎤')
+    # Custom styling
+    st.markdown("""
+    <style>
+    .user-message {
+        background-color: #3E4C59;
+        color: white;
+        padding: 10px;
+        border-radius: 10px;
+        margin: 5px;
+    }
+    .assistant-message {
+        background-color: #FFD700;
+        color: black;
+        padding: 10px;
+        border-radius: 10px;
+        margin: 5px;
+    }
+    .stButton>button {
+        background-color: #007bff;
+        color: pink;
+        font-size: 16px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    
+    st.title('Groq VoiceBot')
+
+    if __name__ == "__main__":
+    main()
